605cf341e85b02988c8c6ea75651a530d880bba1
doom-white <doom.white@yaani.com>
2023-08-06 10:02:30 +0300

    Created & Designed Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BsFillBasketFill } from "react-icons/bs";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const { quantity } = useSelector((state) => state.cart);
   return (
-    <nav>
-      <div>
-        <h3>Kurs Uygulaması</h3>
-        <div>
-          <p>0</p>
+    <nav className="navbar">
+      <h3 className="navbar-title">Kurs Uygulaması</h3>
+      <div className="navbar-cart">
+        <div className="navbar-cart-count">
+          <p>{quantity}</p>
         </div>
         <BsFillBasketFill />
       </div>
